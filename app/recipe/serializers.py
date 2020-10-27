19ca5d6b87069b7db4b2da19ad157d22dee91d2a
Tichalo <ticha.muma@chatdesk.com>
2020-10-27 06:13:18 +0100

    Added filtering for tags and ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -17,8 +17,8 @@ class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
-        fields = ('id', 'name')
-        read_only_fields = ('id',)
+        fields = ('id', 'name', )
+        read_only_fields = ('id', )
 
 
 class RecipeSerializer(serializers.ModelSerializer):
